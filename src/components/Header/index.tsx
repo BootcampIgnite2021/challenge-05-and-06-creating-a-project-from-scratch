5f98f590f92ff84abb67d8b9387c3aaaea9b4be2
Francisco Junior <franciscojn49@gmail.com>
2021-08-31 21:56:39 -0300

    feat: created styles in component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,15 @@
+import Link from 'next/link';
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <div className={styles.contentHeader}>
+      <header className={commonStyles.container}>
+        <Link href="/">
+          <img src="/Logo.svg" alt="Logo" />
+        </Link>
+      </header>
+    </div>
+  );
 }
